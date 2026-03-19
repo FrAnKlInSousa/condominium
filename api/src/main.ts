e90089cc4a442e2ddec6e9e24d54f19838a785d9
franklin <frank.fkn.23@gmail.com>
2026-03-19 12:54:05 -0300

    enable cors local e dominio render

--- api/src/main.ts
+++ api/src/main.ts
@@ -4,7 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+  app.enableCors({
+    origin: ['http://localhost:3000', 'https://condominium-6jtn.onrender.com'],
+  });
 
   await app.listen(process.env.PORT ?? 3001);
 }
