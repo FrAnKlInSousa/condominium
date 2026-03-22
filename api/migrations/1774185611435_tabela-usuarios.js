f119081ec4b0bed40c00f76f614df86c77bd4b31
franklin <frank.fkn.23@gmail.com>
2026-03-22 11:02:23 -0300

    criando tabela de usuarios

---
+++ api/migrations/1774185611435_tabela-usuarios.js
@@ -0,0 +1,45 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+export const shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const up = (pgm) => {
+  pgm.createTable('users', {
+    id: 'id', // mantém padrão do projeto
+    email: {
+      type: 'varchar(255)',
+      notNull: true,
+      unique: true,
+    },
+    password: {
+      type: 'text',
+      notNull: true,
+    },
+    created_at: {
+      type: 'timestamp',
+      default: pgm.func('current_timestamp'),
+    },
+    updated_at: {
+      type: 'timestamp',
+      default: pgm.func('current_timestamp'),
+    },
+    deleted_at: {
+      type: 'timestamp',
+      notNull: false,
+    },
+  });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const down = (pgm) => {
+  pgm.dropTable('users');
+};
