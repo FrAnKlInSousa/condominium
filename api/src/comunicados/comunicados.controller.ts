5c17840ab233a6163f9837cb0ad3a04cf9c48ab5
franklin <frank.fkn.23@gmail.com>
2026-03-22 11:09:26 -0300

    protegendo rotas

--- api/src/comunicados/comunicados.controller.ts
+++ api/src/comunicados/comunicados.controller.ts
@@ -7,10 +7,12 @@ import {
   Put,
   Patch,
   Query,
+  UseGuards,
 } from '@nestjs/common';
 import { ComunicadosService } from './comunicados.service';
 import { CreateInformativoDto } from './dto/create-informativo.dto';
 import { UpdateInformativoDto } from './dto/update-informativo.dto';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 
 @Controller('comunicados')
 export class ComunicadosController {
@@ -26,16 +28,19 @@ export class ComunicadosController {
     return this.service.findAll(search, data, page, limit);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Post()
   create(@Body() body: CreateInformativoDto) {
     return this.service.create(body);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Put(':id')
   update(@Param('id') id: string, @Body() body: UpdateInformativoDto) {
     return this.service.update(Number(id), body);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Patch(':id/delete')
   delete(@Param('id') id: string) {
     return this.service.delete(Number(id));
